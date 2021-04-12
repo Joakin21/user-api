0ff2e4135c796a511458948fa6de44b297dd1d76
Carlos <karloz.97@gmail.com>
2021-04-12 16:16:25 -0300

    Implementación de jwt middleware que protege endpoint /me mediante la validación del token enviado

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -4,18 +4,36 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class JwtMiddleware
 {
     /**
-     * Handle an incoming request.
+     * Verificación de Token de autenticación par el endpoint: Me.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
+
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $header = $request->header('Authorization');
+        if($header == null){
+            return response() -> json(['error' => 'Token not found']);
+        }
+
+        $token = explode(" ", $header)[1];
+        $users = Http::get('https://6071de2050aaea001728541f.mockapi.io/users');
+        $users = json_decode($users, true);
+
+        foreach ($users as $user) {
+            
+            if($user['token'] == $token){
+                $request->replace(['user_id' => $user["id"],'user' => $request ->all(), 'token' => $token]);
+                return $next($request); 
+            }
+        }
+        return response()->json(["error"=>'Invalid token']);
     }
 }
