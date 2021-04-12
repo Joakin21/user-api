c090e325cc454746d3f46aae0dec551e3cd73f71
Carlos <karloz.97@gmail.com>
2021-04-12 14:18:53 -0300

    Se implementa endpoint get /me

--- app/Http/Controllers/MeController.php
+++ app/Http/Controllers/MeController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MeController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('jwt');
+    }
+
+    public function show(Request $request)
+    {
+        $user = $request["user"];
+        $user_id = $user["id"];
+        $user = Http::get('https://6071de2050aaea001728541f.mockapi.io/users/'.$user_id);
+        return $user;
+    }
+    
 }
